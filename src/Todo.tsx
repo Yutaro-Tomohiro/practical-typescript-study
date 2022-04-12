5029b39404e51370a93c1318e7eabcfa0145b462
Yutaro-Tomohiro <50096437+Yutaro-Tomohiro@users.noreply.github.com>
2022-04-12 15:10:27 +0900

    Initial commit

---
+++ src/Todo.tsx
@@ -0,0 +1,10 @@
+import { TodoType } from "./types/todo";
+import { VFC } from "react";
+
+export const Todo: VFC<Pick<TodoType, "title" | "userId" | "completed">> = (
+  props
+) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザー: ${userId})`}</p>;
+};
